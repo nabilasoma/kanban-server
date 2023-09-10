c280258c6e488786742d253f344f819ec7edc06b
unknown <nabilasoma@gmail.com>
2023-09-10 10:15:55 +0600

    added some api

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -17,7 +18,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://<username>:<password>@cluster0.5rfymji.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://kanbanUser:JX0DeFR0oaCSRlAP@cluster0.5rfymji.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,9 +32,55 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const usersCollection = client.db("kanbanDB").collection("kanbanUsers");
+
+
+
+
+    app.post('/kanbanUsers', async(req, res) => {
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    });
+
+    app.get('/kanbanUsers', async(req, res) => {
+        const cursor = usersCollection.find()
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
+
+    app.put('/kanbanUsers/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true };
+        const updateDoc = req.body;
+        const updatedDoc = {
+          $set: {
+            title: updateDoc.title,
+            description: updateDoc.description,
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+      });
+
+
+      app.delete('/kanbanUsers/:id', async(req, res) => {
+        const id =  req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+      })
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
